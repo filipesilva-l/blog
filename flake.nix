739c7edc15fde1b6ecb7ba2ddd76b7cde799f052
Filipe Silva <fil.lourenco.silva@gmail.com>
2024-04-08 19:34:07 -0300

    build: adicionando direnv e nix

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "flake to develop my blog";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_21
+            nodePackages."@astrojs/language-server"
+            nodePackages.prettier
+          ];
+        };
+      });
+}
